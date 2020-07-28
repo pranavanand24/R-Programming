4b2ba6f970ef66e056beba9a28c418f89099e497
Pranav Anand <pranavanand2410@gmail.com>
2020-07-28 16:02:00 +0530

    Add files via upload

---
+++ R Basics.r
@@ -0,0 +1,22 @@
+#print Hello in R
+print("Hello")
+
+#Declare variable in R
+v1 = 1:10
+print(v1)
+
+v2 = 10:43
+print(v2)
+
+#Printing the matrix in R
+m1 = matrix(v1, nrow=2, byrow= TRUE)
+print(m1)
+
+#mathematics point of view of R
+answer <- (((((x<- 1:20)+2)*3)-6)/3)
+print(answer)
+
+#vectors use for typecasting
+v = c(3,4,"Hello")
+print(v)
+
